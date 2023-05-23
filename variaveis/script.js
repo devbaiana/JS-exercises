2b0e8201d60f6fa177e202d1287066cb08500eb3
Nathalia <83174459+devbaiana@users.noreply.github.com>
2023-05-23 10:43:22 -0300

    variaveis

---
+++ variaveis/script.js
@@ -0,0 +1,11 @@
+//Exercícios de variáveis
+// Declarar uma variável com o seu nome
+var nome = "Nathalia";
+// Declarar uma variável com a sua idade
+var idade = 24
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+var comida 
+// Atribuir valor a sua comida favorita
+comida = "feijão";
+// Declarar 5 variáveis diferentes sem valores
+var cidade, viagemPreferida, corDeCabelo, altura, time;
