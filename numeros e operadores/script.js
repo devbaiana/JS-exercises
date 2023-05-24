83328b9425be868f44bbddc41ed7eb5e13093a23
Nathalia <83174459+devbaiana@users.noreply.github.com>
2023-05-24 10:11:25 -0300

    numeros e operadores

---
+++ numeros e operadores/script.js
@@ -0,0 +1,35 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total);
+
+// Crie duas expressões que retornem NaN
+var Quantidade1 = "comprei 3 e depois +";
+var Quantidade2 = '3'
+media = (Quantidade1 + Quantidade2)/2
+console.log(media);
+
+var a = "2";
+var b = "x";
+var c = 4
+soma = (a + b)/4;
+console.log(soma);
+
+// Somar a string '200' com o número 50 e retornar 250
+
+var numero = +"200" + 50;
+console.log(numero)
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+// Como dividir o peso por 2?
+// var numero = '80';
+// var unidade = 'kg';
+// var peso = numero + unidade; // '80kg'
+// var pesoPorDois = peso / 2; // NaN (Not a Number)
+// console.log(pesoPorDois);
+
+var numero = '80';
+var unidade = 'kg';
+var peso = numero; // '80kg'
+var pesoPorDois = peso / 2; 
+console.log(pesoPorDois + unidade);
